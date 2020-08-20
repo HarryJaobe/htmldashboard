3d613fc70e894ae4a421a3b7565ce58e7b525bd7
HarryJaobe <64552642+HarryJaobe@users.noreply.github.com>
2020-08-20 21:40:02 +0500

    final

--- assets/js/material-dashboard.js
+++ assets/js/material-dashboard.js
@@ -15,6 +15,15 @@
 
  */
 
+$(document).ready(function(){
+    $(window).resize(function() {
+
+        ellipses1 = $("#bc1 :nth-child(2)")
+        if ($("#bc1 a:hidden").length >0) {ellipses1.show()} else {ellipses1.hide()}
+        
+    })
+    
+});
 (function() {
   isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
 
@@ -37,6 +46,27 @@
     $('html').addClass('perfect-scrollbar-off');
   }
 })();
+var $star_rating = $('.star-rating .fa');
+
+var SetRatingStar = function() {
+  return $star_rating.each(function() {
+    if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
+      return $(this).removeClass('fa-star-o').addClass('fa-star');
+    } else {
+      return $(this).removeClass('fa-star').addClass('fa-star-o');
+    }
+  });
+};
+
+$star_rating.on('click', function() {
+  $star_rating.siblings('input.rating-value').val($(this).data('rating'));
+  return SetRatingStar();
+});
+
+SetRatingStar();
+$(document).ready(function() {
+
+});
 
 
 var breakCards = true;
